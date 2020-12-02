9753db7680adf56caf36f58a90d97cce913cdc0e
Sapieron <pawelochojec@gmail.com>
2020-12-02 21:29:43 +0100

    Base commit - project compiles and variables get updated from UI

---
+++ Drawer_pl_PL.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>ThemeWidgetForm</name>
+    <message>
+        <location filename="themewidget - Copy.ui" line="14"/>
+        <location filename="themewidget.ui" line="14"/>
+        <source>ThemeWidget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="themewidget - Copy.ui" line="22"/>
+        <location filename="themewidget.ui" line="22"/>
+        <source>Theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="themewidget - Copy.ui" line="32"/>
+        <location filename="themewidget.ui" line="32"/>
+        <source>Animation:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="themewidget - Copy.ui" line="42"/>
+        <location filename="themewidget.ui" line="42"/>
+        <source>Legend:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="themewidget - Copy.ui" line="52"/>
+        <location filename="themewidget.ui" line="52"/>
+        <source>Anti-Aliasing</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
